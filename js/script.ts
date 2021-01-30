16ab3a60b7a192a80c0ab504dcdd1e35fb0929d0
kazuhi-ra <kazuhira4126@gmail.com>
2021-01-30 23:35:28 +0900

    完成

--- js/script.ts
+++ js/script.ts
@@ -1,17 +1,4 @@
-import $ from 'jquery'
 import Vue from 'vue'
+import TodoApp from './TodoApp.vue'
 
-import './mount'
-import { mutations } from './Store'
-
-$(function () {
-  $('#addTodo').on('click', function () {
-    mutations.addTodo()
-  })
-
-  $('#todoList').on('input', '.todo:eq(0)', function () {})
-
-  $('#todoList').on('click', '.delete', function () {
-    mutations.removeTodo($('#todoList').find('.delete').index(this))
-  })
-})
+new Vue(TodoApp).$mount('#app')
